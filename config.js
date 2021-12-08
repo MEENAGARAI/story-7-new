cf88226255e8872aff8c228b3fb42a0ee2729a8d
Meena Garai <68517659+MEENAGARAI@users.noreply.github.com>
2021-12-09 04:53:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSmvJV9LPEVCeEkaT4lLFDffi5wjMANYM",
+  authDomain: "storytelling-6bb2f.firebaseapp.com",
+  databaseURL: "https://storytelling-6bb2f-default-rtdb.firebaseio.com",
+  projectId: "storytelling-6bb2f",
+  storageBucket: "storytelling-6bb2f.appspot.com",
+  messagingSenderId: "353829622993",
+  appId: "1:353829622993:web:e70f402b236c65eb5461f0"
+  };
